f7e84c33956b29d611dcdf62fc9dc22c2554a8cd
Tinotenda Muponda <44811208+TinotendaMuponda@users.noreply.github.com>
2026-04-30 01:29:30 -0700

    feat(icons): add CUSTOM_ELEMENTS_SCHEMA to AppModule
    Allows Angular to accept <cds-icon> custom elements
    without throwing unknown element errors at compile time

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
@@ -11,6 +11,7 @@ import { AppRoutingModule } from './app-routing.module';
   declarations: [AppComponent, ModelsListComponent, ModelDetailComponent],
   imports: [BrowserModule, HttpClientModule, FormsModule, AppRoutingModule],
   providers: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
